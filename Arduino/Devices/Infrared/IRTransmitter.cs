c8059d84b4d7acbc7a0762aea99fdbb602ca94af
billchetch <45006134+billchetch@users.noreply.github.com>
2025-06-14 22:24:45 +0800

    asdf

--- Arduino/Devices/Infrared/IRTransmitter.cs
+++ Arduino/Devices/Infrared/IRTransmitter.cs
@@ -4,14 +4,30 @@ namespace Chetch.Arduino.Devices.Infrared;
 
 public class IRTransmitter : ArduinoDevice
 {
+    #region Fields
+    public Dictionary<String, List<IRData>> sequences = new Dictionary<String, List<IRData>>();
+    #endregion
+
+    #region Constructors
     public IRTransmitter(byte id, String sid, String? name = null) : base(id, sid, name)
     {
         //empty for now
     }
+    #endregion
+
+    #region Methods
+    public List<IRData> CreateSequence(String name)
+    {
+        if (!sequences.ContainsKey(name))
+        {
+            sequences[name] = new List<IRData>();
+        }
+        return sequences[name];
+    }
 
     public void Transmit(IRData data)
     {
-        SendCommand(DeviceCommand.SEND, data.Protocol, data.Address, data.Command);
+        SendCommand(DeviceCommand.SEND, (UInt16)data.Protocol, data.Address, data.Command);
     }
 
     public Task TransmitAsync(List<IRData> data, int delay)
@@ -20,7 +36,7 @@ public class IRTransmitter : ArduinoDevice
         {
             throw new ArgumentException("Delay must be positive");
         }
-        
+
         return Task.Run(() =>
         {
             foreach (var d in data)
@@ -30,4 +46,15 @@ public class IRTransmitter : ArduinoDevice
             }
         });
     }
+
+    public async void TransmitSequence(String name, int delay)
+    {
+        if (!sequences.ContainsKey(name))
+        {
+            throw new ArgumentException(String.Format("Sequence {0} not found", name));
+        }
+
+        await TransmitAsync(sequences[name], delay);
+    }
+    #endregion
 }
