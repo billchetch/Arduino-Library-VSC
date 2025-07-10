d02367bc2a2f717d6d5b700084e8ecd7a6a50e62
billchetch <45006134+billchetch@users.noreply.github.com>
2025-07-10 22:29:00 +0800

    asdfsdf

---
+++ Arduino.Tests/MSTestSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http.Headers;
+using Microsoft.Extensions.Configuration;
+using Chetch.Utilities;
+
+[assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
+
+public static class Settings {
+    public static IConfiguration GetConfig()
+    {
+        return Config.GetConfiguration(["settings.local.json", "settings.json"]);
+    }   
+}
