edaf6955872743b614eec5d466eac6a774d5821c
bill@chetch.net <bill@chetch.net>
2025-08-22 00:06:22 +0800

    asdf

--- Arduino/Devices/Comms/MCP2515.cs
+++ Arduino/Devices/Comms/MCP2515.cs
@@ -4,8 +4,20 @@ namespace Chetch.Arduino.Devices.Comms;
 
 public class MCP2515 : ArduinoDevice
 {
+
+    #region Properties
+    [ArduinoMessageMap(Messaging.MessageType.STATUS_RESPONSE, 1)]
+    public bool CanReceiveMessages { get; internal set; } = false;
+
+    [ArduinoMessageMap(Messaging.MessageType.STATUS_RESPONSE, 2)]
+    public bool CanReceiveErrors { get; internal set; } = false;
+    #endregion
+
+
+    #region Constructors
     public MCP2515(string sid, string? name = null) : base(sid, name)
     {
-        
+
     }
+    #endregion
 }
