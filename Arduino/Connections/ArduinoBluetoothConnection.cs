8c7489f3ebbdd32602fbd919b74fbfa59bc2a28a
billchetch <45006134+billchetch@users.noreply.github.com>
2026-03-30 01:52:53 +0700

    asdf

--- Arduino/Connections/ArduinoBluetoothConnection.cs
+++ Arduino/Connections/ArduinoBluetoothConnection.cs
@@ -8,15 +8,25 @@ namespace Chetch.Arduino.Connections;
 /// <summary>
 /// </summary>
 
-public class ArduinoBluetoothConnection : BluetoothSerialConnection, IConnection
+public class ArduinoBluetoothConnection : IConnection
 {
+    #region Properties
+    public bool IsConnected => throw new NotImplementedException();
+
+    #endregion
+
+    #region Events
+    public event EventHandler<byte[]> DataReceived;
+    public event EventHandler<bool> Connected;
+    #endregion
+
     #region Fields
     IBluetoothManager? bm;
     IBluetoothManager.BluetoothDevice device;
     #endregion
 
     #region Constructors
-    public ArduinoBluetoothConnection(IBluetoothManager.BluetoothDevice device) : base(String.Empty, 9600)
+    public ArduinoBluetoothConnection(IBluetoothManager.BluetoothDevice device)
     {
         switch (device)
         {
@@ -31,17 +41,38 @@ public class ArduinoBluetoothConnection : BluetoothSerialConnection, IConnection
             default:
                 throw new NotImplementedException();
         }
+
+        bm.Ready += (sender, ready) =>
+        {
+            Connected?.Invoke(this, ready);
+        };
+
+        bm.DataReceived += (sender, data) =>
+        {
+            DataReceived?.Invoke(this, data);  
+        };
     }
     #endregion
 
     public void Connect()
     {
-        if (OperatingSystem.IsMacOS())
-        {
-            var di = bm.Search(device);
-            SetDevicePath(di.Path);
-        }
-        
-        base.Connect();
+       bm.Connect(device);
+    }
+
+    public void Disconnect()
+    {
+        bm.Disconnect();
+    }
+
+    public void Reconnect()
+    {
+        bm.Disconnect();
+        Thread.Sleep(500);
+        bm.Connect(device);
+    }
+    
+    public void SendData(byte[] data)
+    {
+        bm.SendData(data);
     }
 }
